7d8bd440f81095cf5e879dc9e5c3eaa574a906fa
thelastandrew <thelastandrew@gmail.com>
2022-11-04 02:00:08 +0300

    feat: add NavLink and active link styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './nav.module.css';
 
 const Nav = () => (
   <nav className={styles.nav}>
     <div className={styles.item}>
-      <Link to='/'>Profile</Link>
+      <NavLink className={({isActive}) => isActive ? styles.active : ''} to='/'>Profile</NavLink>
     </div>
     <div className={styles.item}>
-      <Link to='/dialogs'>Messages</Link>
+      <NavLink className={({isActive}) => isActive ? styles.active : ''} to='/dialogs'>Messages</NavLink>
     </div>
     <div className={styles.item}>
-      <Link to='/news'>News</Link>
+      <NavLink className={({isActive}) => isActive ? styles.active : ''} to='/news'>News</NavLink>
     </div>
     <div className={styles.item}>
-      <Link to='/music'>Music</Link>
+      <NavLink className={({isActive}) => isActive ? styles.active : ''} to='/music'>Music</NavLink>
     </div>
     <div className={styles.item}>
-      <Link to='/settings'>Settings</Link>
+      <NavLink className={({isActive}) => isActive ? styles.active : ''} to='/settings'>Settings</NavLink>
     </div>
   </nav>
 );
