3c55d436af7b0854b0365a30a516fd04ecf55362
thelastandrew <thelastandrew@gmail.com>
2022-11-05 10:23:58 +0300

    feat: add dialogs content

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,25 @@ import styles from './dialogs.module.css';
 
 const Dialogs = () => (
   <>
-    Dialogs
+    <div className={styles.dialogs_items}>
+      <div className={styles.chats}>
+        <div className={`${styles.item} ${styles.active}`}>Anna</div>
+        <div className={styles.item}>Mom</div>
+        <div className={styles.item}>Anton</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>
+          <div className={styles.user_name}>Anna</div>
+          <div className={styles.message_text}>Hello</div>
+        </div>
+        <div className={`${styles.message} ${styles.me}`}>
+          <div className={styles.user_name}>me</div>
+          <div className={styles.message_text}>I love you</div>
+        </div>
+        <textarea className={styles.textarea}></textarea>
+        <button className={styles.send_btn}>Send</button>
+      </div>
+    </div>
   </>
 );
 
