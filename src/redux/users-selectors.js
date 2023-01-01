5c93439c54327694d7f2217963026809dd319e8c
thelastandrew <thelastandrew@gmail.com>
2023-01-01 10:09:45 +0200

    feat: add reselect lib

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,4 +1,7 @@
-export const getUsers = state => state.usersPage.users;
+import { createSelector } from 'reselect';
+
+export const getUsersSelector = state => state.usersPage.users;
+export const getUsers = createSelector(getUsersSelector, users => users.filter(u => true));
 export const getPageSize = state => state.usersPage.pageSize;
 export const getCurrentPage = state => state.usersPage.currentPage;
 export const getIsAbleToDecrease = state => state.usersPage.isAbleToDecrease;
